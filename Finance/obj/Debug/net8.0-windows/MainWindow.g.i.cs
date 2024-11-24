edcdcd986e6d713e2b5d6115ebb266447db96cff
PhatNguyenTT <nguyenphat2095@gmail.com>
2024-11-25 02:51:04 +0700

    Sua giao dien

--- Finance/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ Finance/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -75,7 +75,7 @@ namespace Finance {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Finance;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Finance;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
