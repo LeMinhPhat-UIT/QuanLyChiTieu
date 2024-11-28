7978708a1a1ca5eed3b252c7a7bf38b3cfd27ac1
duc68456 <duc68456@gmail.com>
2024-11-28 21:11:56 +0700

    Add files via upload

---
+++ FinanceDatabase.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Wallet (
+    ID INT PRIMARY KEY IDENTITY(1,1), 
+    WalletName NVARCHAR(100) NOT NULL, 
+    WalletType NVARCHAR(50) NOT NULL, 
+    Money DECIMAL(18,2) NOT NULL DEFAULT 0,
+    UpdateDate DATETIME NOT NULL DEFAULT GETDATE() 
+);
+
+
+CREATE TABLE [Transaction] (
+    ID INT PRIMARY KEY IDENTITY(1,1), 
+    TransactionName NVARCHAR(100) NOT NULL, 
+    Money DECIMAL(18,2) NOT NULL, 
+    TransactionMoneyFlow NVARCHAR(50) NOT NULL, 
+    TransactionCatalog NVARCHAR(50),
+    TransactionBudgetService NVARCHAR(50), 
+    TransactionDate DATETIME NOT NULL, 
+);
