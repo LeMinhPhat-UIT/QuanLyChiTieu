0df6d1ed7df10661c776adac07976e915d15976e
PhatNguyenTT <nguyenphat2095@gmail.com>
2024-12-08 02:25:56 +0700

    update tong the giao dien

--- Finance/DAL/WalletDAL.cs
+++ Finance/DAL/WalletDAL.cs
@@ -11,7 +11,7 @@ namespace DAL
     public class WalletDAL
     {
         private List<Wallet> _wallets = new List<Wallet>();
-        private static readonly string _connectionString = "Data Source=IDEAPAD5PRO;Initial Catalog=FinanceManagement;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+        private static readonly string _connectionString = "Data Source=AAAAA;Initial Catalog=IT008_Project;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
         public static void AddWallet(Wallet wallet)
         {
             string query = @"
